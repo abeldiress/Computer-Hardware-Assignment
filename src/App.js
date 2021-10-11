0dd47cf762cd34feacf0e637824f7fc334e43729
Abel Diress <abeldiress05@gmail.com>
2021-10-11 10:16:35 -0400

    .

--- src/App.js
+++ src/App.js
@@ -28,61 +28,70 @@ function App() {
   };
 
   return (
-    <div className="App">
-      {!end && (
-        <>
-          <h1>Welcome to the Computer Hardware Quiz!</h1>
-          <p>
-            {score} / {num + 1}
-          </p>
-          <h3>{questions[num].question}</h3>
-          <img alt="" style={{ marginBottom: 20 }} src={questions[num].image} />
-          {questions[num].options.map((opt) => {
-            return (
-              <div>
-                <input
-                  type="radio"
-                  id={opt}
-                  name={opt}
-                  value={opt}
-                  onClick={onOptionClick}
-                  checked={answered}
-                />
-                <label for={opt}>{opt}</label>
+    <>
+      <title>Computer hardwre quiz!</title>
+      <div className="App">
+        {!end && (
+          <>
+            <h1>Welcome to the Computer Hardware Quiz!</h1>
+            <p>
+              {score} / {num + 1}
+            </p>
+            <h3>{questions[num].question}</h3>
+            <img
+              alt=""
+              style={{ marginBottom: 20 }}
+              src={questions[num].image}
+            />
+            {questions[num].options.map((opt) => {
+              return (
+                <div>
+                  <input
+                    type="radio"
+                    id={opt}
+                    name={opt}
+                    value={opt}
+                    onClick={onOptionClick}
+                    checked={answered}
+                  />
+                  <label for={opt}>{opt}</label>
+                </div>
+              );
+            })}
+            {answered && correct && (
+              <div class="explanation">
+                <h4>Correct!</h4>
+                <p>{questions[num].explanation}</p>
               </div>
-            );
-          })}
-          {answered && correct && (
-            <div class="explanation">
-              <h4>Correct!</h4>
-              <p>{questions[num].explanation}</p>
-            </div>
-          )}
-          {answered && !correct && (
-            <div class="explanation-wrong">
-              <h4>Incorrect!</h4>
-              <p>{questions[num].explanation}</p>
-            </div>
-          )}
-          <button
-            onClick={onNextClick}
-            style={{
-              marginTop: 20,
-            }}
-          >
-            Next
-          </button>
-        </>
-      )}
-      {end && (
-        <>
-          <h1>Congratulations!</h1>
-          <p>
-            You scored {score} out of {num + 1} questions.
-          </p>
-        </>
-      )}
-    </div>
+            )}
+            {answered && !correct && (
+              <div class="explanation-wrong">
+                <h4>Incorrect!</h4>
+                <p>{questions[num].explanation}</p>
+              </div>
+            )}
+            <button
+              onClick={onNextClick}
+              style={{
+                marginTop: 20,
+                marginBottom: 30,
+                padding: 15,
+              }}
+            >
+              Next
+            </button>
+          </>
+        )}
+        {end && (
+          <>
+            <h1>Congratulations!</h1>
+            <p>
+              You scored {score} out of {num + 1} questions.
+            </p>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
--- src/questions.js
+++ src/questions.js
@@ -1,11 +1,13 @@
 const questions = [
   {
-    question: "What is my name?",
-    options: ["Abel", "Steve", "Bob", "Mati"],
-    answer: "Bob",
+    question:
+      "For a gaming PC, how many cores is recommended for optimal performance?",
+    options: ["12-18", "4-6", "1-2", "Doesn’t matter."],
+    answer: "4-6",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/8/87/Spas_na_Ilyine_-_Patriarch_Abel.jpg",
-    explanation: "my name is Abel...",
+    explanation:
+      "Answer: 4-6 \n\nAlthough gaming PC’s require high frequencies, they don’t necessarily need lots of cores. Gaming PC’s tend to concentrate their processing power into a single task, or very few tasks, therefore, having too many cores will actually hinder performance in most instances. On the other hand, 2 cores is too low as it is known to produce lower FPS rates. CPU’s developed by Ryzen and AMD are specifically designed with fewer cores and high frequency, making them a recommended choice in the gaming industry.",
   },
   {
     question: "What is my birthday?",
